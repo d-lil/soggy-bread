35aa1e6e6606f837736ed9cbb77104c95bdb01ad
Danny <dahneel@gmail.com>
2024-05-29 15:53:49 -0600

    updated some styling on home page and added some images for computer and phone

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,7 +5,8 @@ import { Route, Routes } from "react-router-dom";
 import { useState, useEffect } from "react";
 import certificate from "./assets/certificate.png";
 import rain from "./assets/rain.mp4";
-
+import phoneModel from "./assets/phone_model.png";
+import computerModel from "./assets/laptop_model.png";
 
 
 const Home = () => {
@@ -39,23 +40,30 @@ const Home = () => {
           </div>
           <div className="window-sill-1"></div>
           <div className="window-sill-2"></div>
+          <div className="window-sill-shadow"></div>
         </div>
       </div>
       <div className="lower-room-container">
+      <div className="phone">
+             <Link to ="/phone">
+              <img src={phoneModel} alt="phone model" />
+             </Link>
+          </div>
+          <div className="computer">
+            <Link to="computer">
+              <img src={computerModel} alt="computer model" />
+            </Link>
+          </div>
         <div className="desk-top">
         <div className="desk-left"></div>
         <div className="desk">
-          <div className="phone">
-            <Link to="phone">📞Phone</Link>
-          </div>
+
           <div className="window-video-reflection">
               <video autoPlay loop muted>
                 <source src={rain} type="video/mp4" />
               </video>
             </div>
-          <div className="computer">
-            <Link to="computer">👽Computer</Link>
-          </div>
+
         </div>
         <div className="desk-right"></div>
       </div>
