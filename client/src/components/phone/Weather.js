a2b66524489d7f2c7f20c50361b1aa03ed6f27e5
Danny <dahneel@gmail.com>
2024-06-25 21:02:14 -0600

    adjusted http to https in an api call

--- client/src/components/phone/Weather.js
+++ client/src/components/phone/Weather.js
@@ -76,7 +76,7 @@ const Weather = () => {
 
     const fetchCurrentWeather = async (city) => {
         try {
-            const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
             const response = await fetch(url);
             const data = await response.json();
             if (data.cod !== 200) {
@@ -97,7 +97,7 @@ const Weather = () => {
 
     const fetchForecast = async (city) => {
         try {
-            const url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
+            const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
             const response = await fetch(url);
             const data = await response.json();
             if (data.cod !== "200") {
